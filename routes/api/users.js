cf2d86edc020cf60440295014751d3f2d2ff5aa4
mzsellem-GA <125702378+mzsellem-GA@users.noreply.github.com>
2023-06-21 10:39:48 -0400

    MERN-Stack Infrastructure

---
+++ routes/api/users.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const usersCtrl = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
+
+//POST route to handle a controller function (currently TBD)
+router.post('/', usersCtrl.create)
+//POST route to handle log in controller
+router.post('/login', usersCtrl.login)
+//GET  /api/users/check-token
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
+
+
+//we need to export our router
+module.exports = router
